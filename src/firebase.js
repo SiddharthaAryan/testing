e425229cf6faf5d65880375b28ccf2ca7a7fd1a2
SiddharthaAryan <131577270+SiddharthaAryan@users.noreply.github.com>
2026-07-21 05:47:30 +0530

    Connect app to Firebase project

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: 'AIzaSyByKkSoWgaJNRQhkDPwlql5uRFv4eXWSW4',
+  authDomain: 'nh-certificate-verification.firebaseapp.com',
+  projectId: 'nh-certificate-verification',
+  storageBucket: 'nh-certificate-verification.firebasestorage.app',
+  messagingSenderId: '130997482561',
+  appId: '1:130997482561:web:f2fa80fc408021e3e982c0',
 };
 
 const app = initializeApp(firebaseConfig);
